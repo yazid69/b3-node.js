2ea7890a3a03d2d1a57861dad9d52db17fdf578d
yazid69 <yazidlembirik69@outlook.fr>
2023-01-04 12:07:08 +0100

    feat: Chat App Node.js

--- src/routes/postRegister.ts
+++ src/routes/postRegister.ts
@@ -3,27 +3,29 @@ import bodyParser from "body-parser";
 import { createUser } from "../repositories/userRepository";
 
 export function postRegister(app: Application) {
-    app.post('/register', bodyParser.urlencoded(), async (req, res) => {
-      try {
-        const { name, email } = req.body;
-        if (!name || !email) {
-          res.status(400).send('Bad Request')
-          return  
-  
+  app.post("/register", bodyParser.urlencoded(), async (req, res) => {
+    try {
+      const { name, email } = req.body;
+      console.log(email, name)
+      if (!name || !email) {
+        res.status(400).send("Bad Request");
+        return;
       }
-      
+
       const user = await createUser(name, email);
       if (!user) {
-        res.status(400).send('Email already used')
-        return
+        res.status(400).send("Email already used");
+        return;
       }
-  
-        res.cookie('ssid', user.id,  { signed: true, httpOnly: true, sameSite: true});
-        res.redirect('/');}
-        catch (e) {
-          res.status(500).send('Internal Server Error')
-        }
-      }
-    )
-  }
 
+      res.cookie("ssid", user.id, {
+        signed: true,
+        httpOnly: true,
+        sameSite: true,
+      });
+      res.redirect("/");
+    } catch (e) {
+      res.status(500).send("Internal Server Error");
+    }
+  });
+}
